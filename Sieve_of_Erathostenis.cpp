cff4ff51672e8b02f8a517efee62ce5c651b611c
sj_rifat <45694548+sjRifat@users.noreply.github.com>
2019-09-10 17:03:36 +0600

    Update Sieve_of_Erathostenis.cpp

--- Sieve_of_Erathostenis.cpp
+++ Sieve_of_Erathostenis.cpp
@@ -6,92 +6,36 @@ using namespace std;
 #define		all(p)          p.begin(),p.end()
 #define		zz(v)           v.size()
 #define		ss              ' '
+#define 	S(a)            scanf("%lld",&a)
+#define 	SS(a,b)         scanf("%lld %lld",&a,&b)
+#define 	SSS(a,b,c)      scanf("%lld %lld %lld",&a,&b,&c)
 #define		gcd(a,b)        __gcd(a,b)
 #define		lcm(a,b)        (a*b)/gcd(a,b)
 #define		pi              acos(-1.0)
 typedef		long long       ll;
 typedef		vector<ll>      vll;
 
-
 bool ar[1000010];
-void sive(ll n){
-    vll v;
+vll v;
+void sieve(ll n){
     ll i,j;
-    ll x=sqrt(n);
-    ar[0]=ar[1]=1;
-    for(i=4;i<=n;i+=2) ar[i]=1;
-    for(i=3;i<=x;i+=2){
+    v.pb(2LL);
+    for(i=3;i<=n;i+=2){
         if(ar[i]==0){
-            for(j=i+i;j<=n;j+=i) ar[j]=1;
+            v.pb(i);
+            if(i>n/i) continue;
+            for(j=i*i;j<=n;j+=(i+i)) ar[j]=1;
         }
     }
-    for(i=0;i<=n;i++){
-        if(ar[i]==0) v.pb(i);
-    }
-     for(i=0;i<zz(v);i++){
+}
+
+int main(){
+//    freopen("input.txt","r",stdin);
+    ll a,b,c,i,j,t,k,lie,n,o,x,y,z,ar[200010];
+    sieve(100);
+    for(i=0;i<zz(v);i++){
         cout<<v[i]<<ss;
     }
-}
-
-int main()
-{
-    ll a,b,c,i,j,t,k=1,m,n,o,p,x,y,z;
-    cin>>n;
-    sive(n);
 
     return 0;
 }
-
-
-
-
-************************************************************************
-
-
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-
-
-const int FLAG_SIZE = 1001000;
-const int PRIME_SIZE = 80000;
-
-bool flag[FLAG_SIZE];
-int prime[PRIME_SIZE];
-
-void sieve()
-{
-    int i,j,ind = 1,d;
-    prime[ind++]=2;
-
-    for(i=3;i<FLAG_SIZE;i+=2)
-    {
-        if(flag[i] == false)
-        {
-            prime[ind++]=i;
-
-            if(i > FLAG_SIZE/i) ///cause i*i > FLAG_SIZE
-                continue;
-
-            for(j=i*i,d=i+i;j<=FLAG_SIZE;j+=d)
-                flag[j]=true;
-        }
-    }
-    for(i=1;i<=100;i++)
-    {
-        printf("%d\n",prime[i]);
-    }
-    printf("Number of Prime : %d\n",ind);
-}
-
-
-int main()
-{
-//    freopen("in.txt","r",stdin);
-//    freopen("output.txt","w",stdout);
-
-    sieve();
-
-    return 0;
-}
-
