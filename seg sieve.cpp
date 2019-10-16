4f9a7d9cbc05abde3e0212cded3367f7a6dcc525
sj_rifat <45694548+sjRifat@users.noreply.github.com>
2019-10-16 17:54:34 +0600

    Update seg sieve.cpp

--- seg sieve.cpp
+++ seg sieve.cpp
@@ -1,57 +1,68 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define		pb              push_back
+#define		PB              pop_back
+#define		nn              "\n"
+#define		all(p)          p.begin(),p.end()
+#define		zz(v)           (ll)v.size()
+#define		sp              ' '
+#define 	S(a)            scanf("%lld",&a)
+#define 	SS(a,b)         scanf("%lld %lld",&a,&b)
+#define 	SSS(a,b,c)      scanf("%lld %lld %lld",&a,&b,&c)
+#define		gcd(a,b)        __gcd(a,b)
+#define		lcm(a,b)        (a*b)/gcd(a,b)
+#define		pi              acos(-1.0)
+typedef		long long       ll;
+typedef		vector<ll>      vll;
 
-#define mx  100009
-typedef long long ll;
-vector < int > primes;
+vll v;
+bool ar[1000010];
 
-void sieveOfEratosthenes()
-{
-    bool flag[mx+1];
-    for(int i=0 ; i<=mx ; i++)
-        flag[i]=true;
-    primes.push_back(2);
-
-    flag[0]=flag[1]=false;
-
-    for(int i=4 ; i<=mx ; i+=2)
-        flag[i]=false;
-    for(int i=3 ; i<=mx ; i+=2){
-        if(flag[i]==true) /// i is prime{
-            primes.push_back(i);
-            for(int j=i+i ; j<=mx ; j=j+i)
-                flag[j]=false; ///j is not prime
+void sieve(){
+    ll i,j,n=1000010;
+    v.pb(2LL);
+    for(i=3;i<=n;i+=2){
+        if(ar[i]==0){
+            v.pb(i);
+            if(i>n/i) continue;
+            for(j=i*i;j<=n;j+=(i+i)) ar[j]=1;
         }
     }
 }
 
-void segmentedSieve(ll L, ll R)
+void segmented_sieve(ll L,ll R)
 {
+    ll c=0;
     bool isPrime[R-L+1];
-    for(int i=0 ; i<=R-L+1 ; i++)
+    for(ll i=0;i<=R-L+1;i++)
         isPrime[i]=true;
     if(L==1)
         isPrime[0]=false;
-    for(int i=0 ; primes[i]*primes[i]<=R ; i++){
-        ll curPrime=primes[i];
+    for(ll i=0;v[i]*v[i]<=R;i++){
+        ll curPrime=v[i];
         ll base=curPrime*curPrime;
         if(base<L)
             base=((L+curPrime-1)/curPrime)*curPrime;
-        for(ll j=base ; j<=R ; j+=curPrime)
+        for(ll j=base;j<=R;j+=curPrime)
             isPrime[j-L]=false;
     }
-    for(int i=0 ; i<=R-L ; i++){
+    for(ll i=0;i<=R-L;i++){
         if(isPrime[i]==true)
-            cout<<L+i<<' ';
+            c++;;
     }
-    cout<<endl;
+    printf("%lld\n",c);
 }
+
 int main()
 {
-    sieveOfEratosthenes();
-    ll L,R;
-    scanf("%lld%lld",&L,&R);
-    segmentedSieve(L,R);
-    
+    sieve();
+    ll l,r,t,c=0;
+    S(t);
+    while(t--){
+        SS(l,r);
+        printf("Case %lld: ",++c);
+        segmented_sieve(l,r);
+    }
+
     return 0;
 }
