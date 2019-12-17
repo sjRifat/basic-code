94b48e51cec889f34c50b352f390b8720daa3c18
sj_rifat <45694548+sjRifat@users.noreply.github.com>
2019-12-17 10:56:01 +0600

    Update dijkastra.cpp

--- dijkastra.cpp
+++ dijkastra.cpp
@@ -79,3 +79,91 @@ int main()
 1 4 5
 4 7 6
 */
+
+
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef		long long       ll;
+typedef		vector<ll>      vll;
+#define		pb              push_back
+#define		PB              pop_back
+#define		nn              "\n"
+#define		all(p)          p.begin(),p.end()
+#define		zz(v)           (ll)v.size()
+#define		mms(ar,a)       memset(ar,a,sizeof(ar))
+#define		ss              ' '
+#define 	S(a)            scanf("%lld",&a)
+#define 	SS(a,b)         scanf("%lld %lld",&a,&b)
+#define 	SSS(a,b,c)      scanf("%lld %lld %lld",&a,&b,&c)
+#define		gcd(a,b)        __gcd(a,b)
+#define		lcm(a,b)        (a*b)/gcd(a,b)
+#define		pi              acos(-1.0)
+
+vll g[100];
+ll w[100][100],dist[100],node,edge;
+void zero(){
+    ll i,j;
+    for(i=0;i<node+3;i++){
+        dist[i]=9999999;
+        g[i].clear();
+        for(j=0;j<node+3;j++) w[i][j]=0;
+    }
+}
+
+void dijkastra(ll u){
+    dist[u]=0;
+    ll i,j;
+    priority_queue<pair<ll,ll>>pq;
+    pq.push({-0,-u});
+    while(!pq.empty()){
+        u=-pq.top().second;
+        ll wu=-pq.top().first;
+
+        printf("u = %lld -- wu = %lld\n",u,wu);
+        pq.pop();
+
+        for(i=0;i<zz(g[u]);i++){
+            ll v=g[u][i];
+            ll wv=w[u][v];
+            printf("v = %lld -- wv = %lld\n",v,wv);
+
+            if(wu+wv<dist[v]){
+                dist[v]=wu+wv;
+                pq.push({-dist[v],-v});
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    freopen("input.txt","r",stdin);
+    ll a,b,c,cc=0,d,sm=0,i,j,t,k,lie,m,n,o,x,y,z,zz;
+//    S(t);
+//    while(t--){
+        zero();
+        SS(node,edge);
+        for(i=1;i<=edge;i++){
+            SSS(a,b,c);
+            g[a].pb(b);
+//            v[b].pb(a);
+            w[a][b]=c;
+//            cost[a].pb(c);
+//            cost[b].pb(c);
+        }
+        dijkastra(1);
+        printf("cost is -> %lld\n",dist[3]);
+
+
+//        if(plg==0) printf("Case #%lld: unreachable\n",++cc);
+//        else printf("Case #%lld: %lld\n",++cc,level[finish]);
+//    }
+
+    return 0;
+}
+
